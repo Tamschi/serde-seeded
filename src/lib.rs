c2bc64929c6e03af69f1208772d4053d4dc69c6f
Tamme Schichler <tamme@schichler.dev>
2020-08-15 03:32:54 +0200

    Added FunctionDeSeeder and FunctionSerSeeder

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,43 @@
 pub use serde_seeded_proc_macro_definitions::*;
 
-use serde::{de, ser};
+use {
+    fn_t::Function,
+    serde::{de, ser},
+};
 
 pub trait DeSeeder<T> {
     type Seed: for<'de> de::DeserializeSeed<'de, Value = T>;
     fn seed(self) -> Self::Seed;
 }
 
-pub trait SerSeeder<'a, T> {
-    type Seeded: 'a + ser::Serialize;
-    fn seeded(&'a self, value: &'a T) -> Self::Seeded;
+pub trait SerSeeder<'s, T> {
+    type Seeded: 's + ser::Serialize;
+    fn seeded(&'s self, value: &'s T) -> Self::Seeded;
 }
 
 #[doc(hidden)]
 pub use serde;
+
+#[derive(Debug, Copy, Clone)]
+pub struct FunctionDeSeeder<F: Function>(pub F);
+impl<F: Function<Args = ()>, T> DeSeeder<T> for FunctionSerSeeder<F>
+where
+    F::Output: for<'de> de::DeserializeSeed<'de, Value = T>,
+{
+    type Seed = F::Output;
+    fn seed(self) -> Self::Seed {
+        self.0.call(())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct FunctionSerSeeder<F: Function>(pub F);
+impl<'s: 'a, 'a, F: Function<Args = (&'a T,)>, T: 'a> SerSeeder<'s, T> for FunctionSerSeeder<F>
+where
+    F::Output: 's + ser::Serialize,
+{
+    type Seeded = F::Output;
+    fn seeded(&'s self, value: &'s T) -> Self::Seeded {
+        self.0.call((value,))
+    }
+}
