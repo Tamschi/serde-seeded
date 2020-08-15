85e7b0742f90ad46ee6d1c31476985577b30c751
Tamme Schichler <tamme@schichler.dev>
2020-08-15 19:32:01 +0200

    Added FnDeSeeder and FnSerSeeder. They'll be more useful anyway.

--- src/lib.rs
+++ src/lib.rs
@@ -41,3 +41,25 @@ where
         self.0.call((value,))
     }
 }
+
+#[derive(Debug, Copy, Clone)]
+pub struct FnDeSeeder<F>(pub F);
+impl<'de, Seed: de::DeserializeSeed<'de>, F: Fn() -> Seed> DeSeeder<'de, Seed::Value>
+    for FnDeSeeder<F>
+{
+    type Seed = Seed;
+    fn seed(self) -> Self::Seed {
+        self.0()
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct FnSerSeeder<F>(pub F);
+impl<'s, F: Fn(&'s T) -> Seeded, T: 's, Seeded: 's + ser::Serialize> SerSeeder<'s, T>
+    for FnSerSeeder<F>
+{
+    type Seeded = Seeded;
+    fn seeded(&'s self, value: &'s T) -> Self::Seeded {
+        self.0(value)
+    }
+}
