1f3104192f57ecac10a2a974d57cf92a4a891118
Tamme Schichler <tamme@schichler.dev>
2020-08-15 19:36:37 +0200

    Removed Function*Seeders and dependency on fn-t

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 pub use serde_seeded_proc_macro_definitions::*;
 
-use {
-    fn_t::Function,
-    serde::{de, ser},
-};
+use serde::{de, ser};
 
 pub trait DeSeeder<'de, T> {
     type Seed: de::DeserializeSeed<'de, Value = T>;
@@ -17,30 +14,6 @@ pub trait SerSeeder<'s, T> {
 
 #[doc(hidden)]
 pub use serde;
-
-#[derive(Debug, Copy, Clone)]
-pub struct FunctionDeSeeder<F>(pub F);
-impl<'de, F: Function<Args = ()>, T> DeSeeder<'de, T> for FunctionSerSeeder<F>
-where
-    F::Output: de::DeserializeSeed<'de, Value = T>,
-{
-    type Seed = F::Output;
-    fn seed(self) -> Self::Seed {
-        self.0.call(())
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-pub struct FunctionSerSeeder<F>(pub F);
-impl<'s: 'a, 'a, F: Function<Args = (&'a T,)>, T: 'a> SerSeeder<'s, T> for FunctionSerSeeder<F>
-where
-    F::Output: 's + ser::Serialize,
-{
-    type Seeded = F::Output;
-    fn seeded(&'s self, value: &'s T) -> Self::Seeded {
-        self.0.call((value,))
-    }
-}
 
 #[derive(Debug, Copy, Clone)]
 pub struct FnDeSeeder<F>(pub F);
