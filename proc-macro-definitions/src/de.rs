2a0d932baffba594abb29d9bb6bfb0c23424ee3c
Tamme Schichler <tamme@schichler.dev>
2020-08-16 18:09:10 +0200

    Added about half of full generics support for deserialisation

--- proc-macro-definitions/src/de.rs
+++ proc-macro-definitions/src/de.rs
@@ -1,15 +1,15 @@
 use crate::{serde_seeded, shared::ensure_second};
 use call2_for_syn::call2;
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::{quote, quote_spanned, ToTokens};
 use std::borrow::Cow;
 use syn::{
 	parenthesized, parse2,
 	punctuated::{Pair, Punctuated},
 	spanned::Spanned as _,
-	Data, DeriveInput, Error, FnArg, GenericParam, Generics, Ident, PatType, Token,
+	Data, DeriveInput, Error, FnArg, GenericParam, Generics, Ident, Lifetime, PatType, Token,
 };
-use wyz::Pipe as _;
+use wyz::{Pipe as _, TapOption as _};
 
 pub fn expand_derive(input: &DeriveInput) -> syn::Result<TokenStream> {
 	let name = &input.ident;
@@ -48,12 +48,31 @@ pub fn expand_derive(input: &DeriveInput) -> syn::Result<TokenStream> {
 			.ok()
 		})
 		.collect::<Vec<_>>();
-	// fn_generics_lifetimes
+	let fn_generics_lifetimes = fn_generics
+		.iter()
+		.flat_map(|g| g.lifetimes())
+		.collect::<Vec<_>>();
+	let fn_generics_lifetime_lifetimes = fn_generics_lifetimes
+		.iter()
+		.map(|l| &l.lifetime)
+		.collect::<Vec<_>>();
 	let fn_generics_types = fn_generics
 		.iter()
 		.flat_map(|g| g.type_params())
 		.collect::<Vec<_>>();
+	let fn_generics_type_idents = fn_generics_types
+		.iter()
+		.map(|t| &t.ident)
+		.collect::<Vec<_>>();
 	// fn_generics_consts
+
+	let mut default_de = vec![Lifetime::new("'de", Span::mixed_site())];
+	let de = fn_generics_lifetime_lifetimes
+		.iter()
+		.copied()
+		.find(|l| l.ident == "de")
+		.tap_some(|_| default_de.pop().unwrap())
+		.unwrap_or_else(|| default_de.first().unwrap());
 
 	let args = input
 		.attrs
@@ -77,7 +96,10 @@ pub fn expand_derive(input: &DeriveInput) -> syn::Result<TokenStream> {
 	let arg_names = args
 		.iter()
 		.map(|arg| match arg {
-			FnArg::Receiver(r) => todo!("Error more nicely on receivers in seed_args."),
+			FnArg::Receiver(r) => {
+				Error::new_spanned(r, "self-parameters are not supported in this position")
+					.to_compile_error()
+			}
 			FnArg::Typed(PatType { pat, .. }) => pat.into_token_stream(),
 		})
 		.collect::<Vec<_>>();
@@ -152,34 +174,71 @@ pub fn expand_derive(input: &DeriveInput) -> syn::Result<TokenStream> {
 			}
 			let len = field_idents.len();
 
-			Ok(quote! {
+			Ok(quote_spanned! {Span::mixed_site()=>
 				#(#errors)*
 				#[automatically_derived]
-				impl #name<
-					#(#type_generics_types,)*
-				> {
-					pub fn seed<'de>(#(#args),*) -> impl #serde_seeded::serde::de::DeserializeSeed<'de, Value = Self> {
+				impl<
+					#(#type_generics_types)*
+				> #name<
+					#(#type_generics_type_idents,)*
+				> #type_generics_where {
+					pub fn seed<
+						#(#default_de,)*
+						#(#fn_generics_lifetimes,)*
+						#(#fn_generics_types,)*
+					>(#(#args),*) -> impl #serde_seeded::serde::de::DeserializeSeed<#de, Value = Self> {
 						use #serde_seeded::{
 							DeSeeder as _,
 							SerSeeder as _,
 							serde::de,
 						};
 
-						struct Seed {#(#args),*};
-						impl<'de> de::DeserializeSeed<'de> for Seed {
-							type Value = #name;
-							fn deserialize<D: de::Deserializer<'de>>(self, deserializer: D) -> ::std::result::Result<Self::Value, D::Error> {
-								struct Visitor {#(#args),*};
-								impl<'de> de::Visitor<'de> for Visitor {
-									type Value = #name;
+						struct Seed<
+							#(#fn_generics_lifetimes,)*
+							#(#type_generics_types,)*
+							#(#fn_generics_types,)*
+							> {#(#args),*};
+						impl<
+							#(#default_de,)*
+							#(#fn_generics_lifetimes,)*
+							#(#type_generics_types,)*
+							#(#fn_generics_types,)*
+							> de::DeserializeSeed<#de> for Seed<
+								#(#fn_generics_lifetime_lifetimes,)*
+								#(#type_generics_type_idents,)*
+								#(#fn_generics_type_idents,)*
+							> #type_generics_where {
+							type Value = #name<#(#type_generics_type_idents)*>;
+							fn deserialize<D: de::Deserializer<#de>>(self, deserializer: D) -> ::std::result::Result<Self::Value, D::Error> {
+								struct Visitor<
+									#(#fn_generics_lifetimes,)*
+									#(#type_generics_types,)*
+									#(#fn_generics_types,)*
+								> {#(#args),*};
+								impl<
+									#(#default_de,)*
+									#(#fn_generics_lifetimes,)*
+									#(#type_generics_types,)*
+									#(#fn_generics_types,)*
+								> de::Visitor<#de> for Visitor<
+									#(#fn_generics_lifetime_lifetimes,)*
+									#(#type_generics_type_idents,)*
+									#(#fn_generics_type_idents,)*
+								> #type_generics_where {
+									type Value = #name<
+										#(#type_generics_type_idents,)*
+									>;
 									fn expecting(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
 										write!(f, stringify!(#name))
 									}
 
-									fn visit_seq<A: de::SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
+									fn visit_seq<A: de::SeqAccess<#de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
 										let Self {#(#arg_names),*} = self;
+
+										#(let #field_idents = seq.#nexts?.ok_or_else(|| de::Error::invalid_length(0, &self))?;)*
+
 										Ok(#name {
-											#(#field_idents: seq.#nexts?.ok_or_else(|| de::Error::invalid_length(0, &self))?,)*
+											#(#field_idents,)*
 										})
 									}
 								}
