e138dae0314063db661f4df2ee12da0d6ed0d21a
Tamme Schichler <tamme@schichler.dev>
2020-08-19 22:32:27 +0200

    shelved/another-try-at-less-indirection

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
 pub use serde_seeded_proc_macro_definitions::*;
 
-use erased_serde as eser;
-use serde::de;
+use serde::{de, ser};
 
 pub trait DeSeeder<'de, T> {
 	type Seed: de::DeserializeSeed<'de, Value = T>;
 	fn seed(self) -> Self::Seed;
 }
 
-pub trait SerSeeder<T> {
-	// This would be nicer with a generic associated type, but that being ready seems a while off.
-	fn seeded<'s>(&'s self, value: &'s T) -> Seeded<'s>;
-}
-pub type Seeded<'s> = Box<dyn 's + eser::Serialize>;
-
-impl<S: SerSeeder<T>, T> SerSeeder<T> for &S {
-	fn seeded<'s>(&'s self, value: &'s T) -> Seeded<'s> {
-		S::seeded(self, value)
-	}
+pub trait SerSeeder<'ser, T, Seeded: 'ser + ser::Serialize> {
+	type Seeded: 'ser + ser::Serialize;
+	fn seeded(self, value: &'ser T) -> Seeded;
 }
 
 #[doc(hidden)]
@@ -38,15 +30,17 @@ impl<'de, Seed: de::DeserializeSeed<'de>, F: Fn() -> Seed> DeSeeder<'de, Seed::V
 pub struct FnSerSeeder<F>(pub F);
 /// The struct constructor doesn't always coerce closures correctly, but this here does.
 impl<F> FnSerSeeder<F> {
-	pub fn new<T>(f: F) -> Self
+	pub fn new<T, S>(f: F) -> Self
 	where
-		F: for<'a> Fn(&'a T) -> Seeded<'a>,
+		F: Fn(&T) -> S,
+		S: ser::Serialize,
 	{
 		Self(f)
 	}
 }
-impl<F: for<'a> Fn(&'a T) -> Seeded<'a>, T> SerSeeder<T> for FnSerSeeder<F> {
-	fn seeded<'s>(&'s self, value: &'s T) -> Box<dyn 's + eser::Serialize> {
+impl<'ser, F: Fn(&T) -> S, S: 'ser + ser::Serialize, T> SerSeeder<'ser, T, S> for FnSerSeeder<F> {
+	type Seeded = S;
+	fn seeded(self, value: &'ser T) -> S {
 		self.0(value)
 	}
 }
